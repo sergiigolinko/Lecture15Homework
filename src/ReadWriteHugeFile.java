886a33a31c22f65ff19e275f8e1951ddf5766304
sergii <sergii.glavmarket.com@gmail.com>
2019-12-26 22:29:13 +0200

    write hashmap to file

--- src/ReadWriteHugeFile.java
+++ src/ReadWriteHugeFile.java
@@ -4,41 +4,37 @@ import java.util.Map;
 
 public class ReadWriteHugeFile {
     public static void main(String[] args) throws Exception {
-        Map<String,Integer> map=new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         FileOutputStream fileOutputStream = new FileOutputStream("output.txt");
-        ObjectOutputStream oos = new ObjectOutputStream(fileOutputStream);
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\Сергей\\IdeaProjects\\Lecture15Homework\\src\\1.txt")));
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\Сергей\\IdeaProjects\\Lecture15Homework\\src\\big.txt")));
         StringBuilder stringBuilder = new StringBuilder();
         boolean nextLineExists = true;
         while (nextLineExists) {
             String currentLine = reader.readLine();
-            String[] words=currentLine.split(" ");
-            for (String s:words
-                 ) {
-                if(map.containsKey(s)){
-                    map.put(s,map.get(s)+1);
-                }
-                else {
-                    map.put(s,1);
+            String[] words = currentLine.split(" ");
+            for (String s : words
+            ) {
+                if (map.containsKey(s)) {
+                    map.put(s, map.get(s) + 1);
+                } else {
+                    map.put(s, 1);
                 }
             }
-            /*byte[] bytesToWrite = map.getBytes();
-            fileOutputStream.write(bytesToWrite);*/
-            oos.writeObject(map);
+            for (Map.Entry<String, Integer> entry : map.entrySet()
+            ) {
+                String readline2 = " " + entry.getKey() + " " + "=" + " " + entry.getValue() + " ";
+                byte[] bytesToWrite = readline2.getBytes();
+                fileOutputStream.write(bytesToWrite);
+            }
 
-
-            //System.out.println(map);
             if (currentLine != null) {
                 stringBuilder.append(currentLine).append("\n");
             } else {
                 nextLineExists = false;
             }
         }
-        oos.close();
         fileOutputStream.close();
-        System.out.println(stringBuilder.toString());
-        System.out.println(oos.toString());
-
     }
 }
